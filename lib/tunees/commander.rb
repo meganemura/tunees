7ea687626c609244cd4d9ee842b0b35aea1024f2
meganemura <meganemura@users.noreply.github.com>
2015-11-05 01:14:00 +0900

    Use ExecJS::Runtimes::JXA to execute scripts

--- lib/tunees/commander.rb
+++ lib/tunees/commander.rb
@@ -1,11 +1,10 @@
+require 'execjs/runtimes/jxa'
+ExecJS.runtime = ExecJS::Runtimes::JXA
+
 module Tunees
   class Commander
     def self.run(script)
-      IO.popen("osascript -l JavaScript", "r+") do |io|
-        io.puts script
-        io.close_write
-        io.gets
-      end
+      ExecJS.exec(script)
     end
   end
 end
